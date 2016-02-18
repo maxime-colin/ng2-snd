c1513e65bfe492540b995384395fcc313e0f43b1
Maxime Colin <maxime.colin2+travisci@gmail.com>
2016-02-18 23:07:56 +0000

    rebuild pages at e6d166e

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+	<head>
+		<title>Angular2 Starter</title>
+
+	<!-- 	<base href="/Projets/ng2-snd/build/" /> -->
+		<base href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="main.css" />
+
+	</head>
+
+	<body>
+		<my-app>App is loading...</my-app>
+		
+		<script src="app/libs/paper-full.js"></script>
+		<script src="app/libs/two.js"></script>
+		<script src="app/libs/rhill-voronoi-core.js"></script>
+		<script src="bundle.js"></script>
+	</body>
+
+</html>
