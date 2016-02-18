fd35d22224e4a258e215100c14028e0ee73e67e3
Maxime <maxime.colin2@gmail.com>
2016-02-19 00:15:05 +0100

    Test travis

--- src/index.php
+++ src/index.php
@@ -1,22 +1,2 @@
-<html>
-
-	<head>
-		<title>Angular2 Starter</title>
-
-	<!-- 	<base href="/Projets/ng2-snd/build/" /> -->
-		<base href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="main.css" />
-
-	</head>
-
-	<body>
-		<my-app>App is loading...</my-app>
-		
-		<script src="app/libs/paper-full.js"></script>
-		<script src="app/libs/two.js"></script>
-		<script src="app/libs/rhill-voronoi-core.js"></script>
-		<script src="bundle.js"></script>
-	</body>
-
-</html>
+<?php
+echo str_replace('##BASE_URL##', dirname($_SERVER['PHP_SELF']) . '/', file_get_contents('index.template.html'));
