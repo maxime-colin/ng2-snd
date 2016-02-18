2465ece68585df219ad6513eb7cd751db4a29de6
Maxime <maxime.colin2@gmail.com>
2016-02-19 00:03:51 +0100

    Test travis

--- Gulpfile.js
+++ Gulpfile.js
@@ -37,7 +37,31 @@ gulp.task('less', function () {
 		.pipe(gulp.dest('./build'));
 });
 
+gulp.task('scriptsDist', function() {
+	return gulp.src('./src/app.ts')
+		.pipe(webpack(require('./webpack.config.js')))
+		.on('error', function handleError() {
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./build'))
+});
+
+gulp.task('copyDist', function(params) {
+	return gulp.src(['./src/**/**.*', '!./src/**/**.ts', '!./src/styles/**'], {
+			base: './src',
+			dot: true
+		})
+		.pipe(gulp.dest('./build'))
+});
+
+gulp.task('lessDist', function () {
+	return gulp.src('./src/styles/main.less')
+		.pipe(less())
+		.pipe(gulp.dest('./build'));
+});
+
 gulp.task('build', ['scripts', 'copy', 'less']);
+gulp.task('dist', ['scriptsDist', 'copyDist', 'lessDist']);
 
 gulp.task('default', ['build'], function() {
 	livereload.listen();
