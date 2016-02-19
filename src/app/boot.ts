af8adec8fb6c9c0fdbdd19e2b877c175364d0cbe
Maxime <maxime.colin2@gmail.com>
2016-02-19 08:27:19 +0100

    PushState => Hash

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -10,7 +10,12 @@ import 'angular2/bundles/angular2-polyfills';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {MyAppComponent} from './app.component';
+import {LocationStrategy} from "angular2/router";
+import {HashLocationStrategy} from "angular2/router";
+import {provide} from "angular2/core";
+
 
 bootstrap(MyAppComponent, [
-	ROUTER_PROVIDERS
+	ROUTER_PROVIDERS,
+	provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
