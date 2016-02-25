b5f825bd0a41acdbaec80d34702d0313a26e9e23
Maxime <maxime.colin2@gmail.com>
2016-02-26 00:09:02 +0100

    Loader

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -6,9 +6,7 @@
 /// <reference path="../typings/lodash.d.ts" />
 /// <reference path="../typings/audio-context.d.ts" />
 
-
 import 'angular2/bundles/angular2-polyfills';
-
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {MyAppComponent} from './app.component';
